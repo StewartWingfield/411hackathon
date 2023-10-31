a15c298b581e997ec6af5d7c79a8fa5cf929c8e1
Stewart Wingfield <sww915@gmail.com>
2023-10-31 14:03:43 -0400

    setup fetch and article card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import ArticleCard from "./ArticleCard";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+
+  const fetchArticles = () => {
+    axios
+      .get("http://hn.algolia.com/api/v1/search?tags=front_page")
+      .then((res) => {
+        /* console.log(res.data.hits); */
+        setArticles(res.data.hits);
+      });
+  };
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  console.log(articles);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {articles.map((article) => (
+        <ArticleCard key={article.story_id} article={article} />
+      ))}
     </div>
   );
 }
